50c9eb9bac92c5c6c38dfd5f78ef0a221f348fed
Sergey Bachinin <sbachinin@yandex.ru>
2023-10-09 18:37:55 +0700

    init

---
+++ sources.mjs
@@ -0,0 +1,18 @@
+export const sources = {
+    "contours": {
+        "url": "https://api.maptiler.com/tiles/contours/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
+        "type": "vector"
+    },
+    "terrain_rgb": {
+        "url": "https://api.maptiler.com/tiles/terrain-rgb-v2/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
+        "type": "raster-dem"
+    },
+    "maptiler_planet": {
+        "url": "https://api.maptiler.com/tiles/v3/tiles.json?key=9C8cs9qKiCW3wSWdeUrN",
+        "type": "vector"
+    },
+    "maptiler_attribution": {
+        "attribution": "<a href=\"https://www.maptiler.com/copyright/\" target=\"_blank\">&copy; MapTiler</a> <a href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\">&copy; OpenStreetMap contributors</a>",
+        "type": "vector"
+    }
+}
