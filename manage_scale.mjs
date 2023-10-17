b49298f191fe79e947a7bfeefe5b5904634ecc81
Sergey Bachinin <sbachinin@yandex.ru>
2023-10-17 14:23:02 +0700

    fix scale items count

--- manage_scale.mjs
+++ manage_scale.mjs
@@ -1,26 +1,35 @@
 let root = document.documentElement
 
 const possible_steps = [
-    1, 5, 10, 25, 50, 100, 250, 500, 1000, 2000, 5000
+    1, 2, 5, 10, 25, 50, 100, 250, 500, 1000, 2000, 5000
 ]
 
 const scale_item_labels = []
 
+const MAX_SCALE_ITEM_WIDTH = 200
+const MIN_SCALE_ITEM_WIDTH = 80
+const MAX_SCALE_ITEMS_COUNT = 15
+const items_viewport_limit = Math.ceil(window.innerWidth / MIN_SCALE_ITEM_WIDTH)
+const max_scale_items_count = Math.min(items_viewport_limit, MAX_SCALE_ITEMS_COUNT)
 
 const update_scale = (fullwidth_scale_width, native_scale_el) => {
 
     // 1. set width
     const ratio = fullwidth_scale_width / (parseFloat(native_scale_el.style.width) - 4) // 4 is 2 borders
-    const native_scale_number = parseInt(native_scale_el.innerText, 10)
-    const fullwidth_scale_number = native_scale_number * ratio
+    const native_scale_value = parseInt(native_scale_el.innerText, 10)
+    const fullwidth_scale_value = native_scale_value * ratio
 
-    const max_number_step = fullwidth_scale_number / (fullwidth_scale_width / 200)
+    // For performance sake, max_scale_items_count takes precedence:
+    // if screen is very wide, items will be wider than MAX_SCALE_ITEM_WIDTH
+    const max_items_count = Math.max(
+        fullwidth_scale_width / MAX_SCALE_ITEM_WIDTH,
+        max_scale_items_count
+    )
+    const min_step_value = fullwidth_scale_value / max_items_count
 
-    let step_num = possible_steps.find((_, i) => {
-        return (i === possible_steps.length - 1
-            || possible_steps[i + 1] > max_number_step)
-    })
-    const step_count = fullwidth_scale_number / step_num
+    let step_value = possible_steps.find(s => s > min_step_value)
+
+    const step_count = fullwidth_scale_value / step_value
     const step_width = fullwidth_scale_width / step_count
 
     root.style.setProperty('--scale-step-width', step_width + 'px')
@@ -30,13 +39,13 @@ const update_scale = (fullwidth_scale_width, native_scale_el) => {
     let scale_units = native_scale_el.innerText.match(/[A-Za-z]+$/)[0]
     
     // prevent change from '1000m' to '1km', use only '1km'
-    if (step_num === 1000 && scale_units === 'm') {
-        step_num = 1
+    if (step_value === 1000 && scale_units === 'm') {
+        step_value = 1
         scale_units = 'km'
     }
 
     scale_item_labels.forEach((l, i) => {
-        let text = step_num * (i + 1)
+        let text = step_value * (i + 1)
         if (i === 0) {
             text += ' ' + scale_units
         }
@@ -71,7 +80,7 @@ export const manage_scale = () => {
     fullwidth_scale_wr.style.display = 'block'
     const fullwidth_scale_width = fullwidth_scale_wr.getBoundingClientRect().width
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < max_scale_items_count; i++) {
         fullwidth_scale_wr.firstElementChild.appendChild(create_scale_item(i))
     }
     
