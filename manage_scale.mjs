79b40e67d9d2cf1ceeacc10bf49177443db2affc
Sergey Bachinin <sbachinin@yandex.ru>
2023-10-18 17:07:46 +0700

    update measurements & update scale on resize

--- manage_scale.mjs
+++ manage_scale.mjs
@@ -10,10 +10,15 @@ const MIN_SCALE_ITEM_WIDTH = 80
 const MAX_SCALE_ITEMS_COUNT = 15
 
 const fullwidth_scale_wr = document.querySelector('.fullwidth-scale-wrapper')
-// TODO needs update on resize
-const fullwidth_scale_width = fullwidth_scale_wr.clientWidth // clientWidth doesn't include padding, it's important for precision
-const items_viewport_limit = Math.ceil(fullwidth_scale_width / MIN_SCALE_ITEM_WIDTH)
-const max_scale_items_count = Math.min(items_viewport_limit, MAX_SCALE_ITEMS_COUNT)
+let fullwidth_scale_width, items_viewport_limit, max_scale_items_count
+
+const measure_base_values = () => {
+    fullwidth_scale_width = fullwidth_scale_wr.clientWidth // clientWidth doesn't include padding, it's important for precision
+    items_viewport_limit = Math.ceil(fullwidth_scale_width / MIN_SCALE_ITEM_WIDTH)
+    max_scale_items_count = Math.min(items_viewport_limit, MAX_SCALE_ITEMS_COUNT)
+}
+
+measure_base_values()
 
 const update_scale = (native_scale_el) => {
 
@@ -21,7 +26,7 @@ const update_scale = (native_scale_el) => {
     const ratio = fullwidth_scale_width / (parseFloat(native_scale_el.style.width))
     const native_scale_value = parseInt(native_scale_el.innerText, 10)
     const fullwidth_scale_value = native_scale_value * ratio
-    
+
     const min_step_value = fullwidth_scale_value / max_scale_items_count
 
     let step_value = possible_steps.find(s => s > min_step_value)
@@ -34,7 +39,7 @@ const update_scale = (native_scale_el) => {
 
     // 2. set labels & hide what's far on the right
     let scale_units = native_scale_el.innerText.match(/[A-Za-z]+$/)[0]
-    
+
     // prevent change from '1000m' to '1km', use only '1km'
     if (step_value === 1000 && scale_units === 'm') {
         step_value = 1
@@ -43,12 +48,12 @@ const update_scale = (native_scale_el) => {
 
     scale_item_labels.forEach((l, i) => {
         const value = step_value * (i + 1)
- 
+
         if (i >= (visible_steps_count - 1)) {
             l.parentElement.classList.add('hidden')
             return
         }
-        
+
         l.parentElement.classList.remove('hidden')
 
         if (i >= (visible_steps_count - 1.2)) {
@@ -91,7 +96,7 @@ export const manage_scale = () => {
     for (let i = 0; i < max_scale_items_count; i++) {
         fullwidth_scale_wr.firstElementChild.appendChild(create_scale_item(i))
     }
-    
+
     update_scale(native_scale_el)
 
     const observer = new MutationObserver(() => update_scale(native_scale_el))
@@ -99,4 +104,25 @@ export const manage_scale = () => {
         native_scale_el,
         { attributes: true, childList: true, subtree: true }
     )
+
+    new ResizeObserver(
+        debounce(
+            () => {
+                measure_base_values()
+                update_scale(native_scale_el)
+            }
+        )
+    ).observe(document.body)
+}
+
+
+function debounce(func, timeout = 100) {
+    let timer
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout(
+            () => { func() },
+            timeout
+        )
+    }
 }
